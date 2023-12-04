c5eb13a4430d4c355349af1316f4eaf379f91787
Daniel Portugal <dpcosta@gmail.com>
2023-12-04 11:39:20 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Web/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using ScreenSound.Web;
+using ScreenSound.Web.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddTransient<ArtistaAPI>();
+
+builder.Services.AddTransient<MusicaAPI>();
+
+builder.Services.AddHttpClient("API",client => {
+    client.BaseAddress = new Uri(builder.Configuration["APIServer:Url"]!);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+
+
+await builder.Build().RunAsync();
