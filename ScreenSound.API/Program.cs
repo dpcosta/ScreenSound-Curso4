c41838290262c570b859623c646c4daaaba2ebc6
Daniel Portugal <dpcosta@gmail.com>
2023-12-12 11:13:52 -0300

    Mudanças no vídeo

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -21,6 +21,8 @@ builder.Services
     .AddIdentityApiEndpoints<PessoaComAcesso>()
     .AddEntityFrameworkStores<ScreenSoundContext>();
 
+builder.Services.AddAuthorization();
+
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
 builder.Services.AddTransient<DAL<Genero>>();
@@ -46,6 +48,7 @@ var app = builder.Build();
 app.UseCors("wasm");
 
 app.UseStaticFiles();
+app.UseAuthorization();
 
 app.AddEndPointsArtistas();
 app.AddEndPointsMusicas();
