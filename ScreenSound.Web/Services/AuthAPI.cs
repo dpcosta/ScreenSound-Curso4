e05d52d8c058ab106d9635dc9e18ad1822f83fb6
Daniel Portugal <dpcosta@gmail.com>
2023-12-12 15:36:01 -0300

    Mudanças no vídeo

--- ScreenSound.Web/Services/AuthAPI.cs
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -7,10 +7,12 @@ namespace ScreenSound.Web.Services;
 
 public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
 {
+    private bool autenticado = false;
     private readonly HttpClient _httpClient = factory.CreateClient("API");
 
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
+        autenticado = false;
         var pessoa = new ClaimsPrincipal();
         var response = await _httpClient.GetAsync("auth/manage/info");
 
@@ -25,6 +27,7 @@ public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
 
             var identity = new ClaimsIdentity(dados, "Cookies");
             pessoa = new ClaimsPrincipal(identity);
+            autenticado = true;
         }
 
         return new AuthenticationState(pessoa);
@@ -45,5 +48,17 @@ public class AuthAPI(IHttpClientFactory factory) : AuthenticationStateProvider
         }
 
         return new AuthResponse { Sucesso = false, Erros = ["Login/senha inválidos"] };
+    }
+
+    public async Task LogoutAsync()
+    {
+        await _httpClient.PostAsync("auth/logout", null);
+        NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+    }
+
+    public async Task<bool> VerificaAutenticado()
+    {
+        await GetAuthenticationStateAsync();
+        return autenticado;
     }
 }
